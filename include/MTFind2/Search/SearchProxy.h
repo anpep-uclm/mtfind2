0bfbb19d6705af38e5354d9fdff471efacab13c8
angel <angel@ttm.sh>
2021-06-20 20:38:53 +0200

    SearchProxy: almost complete implementation
    SearchProxy now enqueues/dequeues jobs correctly based on priority and
    standard-to-premium ratio. Search requests are distributed among all
    available services by using a first-come-first-serve approach: every
    time serve() is called on the program control loop, a thread is spawned
    for each search service available and every search thread removes a
    request from the queue and serves it concurrently.

--- include/MTFind2/Search/SearchProxy.h
+++ include/MTFind2/Search/SearchProxy.h
@@ -21,18 +21,18 @@
 #include <queue>
 #include <tuple>
 
-#include "MTFind2/Client/Client.h"
+#include "../Client/Client.h"
 #include "SearchProvider.h"
 #include "SearchRequest.h"
 #include "SearchService.h"
 
 namespace mtfind2 {
-using SearchTask = std::tuple<Client &, const SearchRequest &>;
+using SearchTask = std::pair<Client *, const SearchRequest *>;
 
 struct SearchTaskCompare final {
     bool operator()(const SearchTask &lhs, const SearchTask &rhs)
     {
-        return std::get<1>(lhs).timestamp() < std::get<1>(rhs).timestamp();
+        return std::get<1>(lhs)->timestamp() < std::get<1>(rhs)->timestamp();
     }
 };
 
@@ -45,24 +45,78 @@ struct SearchTaskCompare final {
  * manage compute and memory resources, see the SearchProxy class.
  */
 struct SearchProxy final : private SearchProvider {
-    void query(const Client &client, const SearchRequest &search_request)
+    void query(Client &client, const SearchRequest &search_request)
     {
+        const std::scoped_lock lock(m_queue_locks[client.subscription_type()]);
+        m_queues[client.subscription_type()].push(SearchTask(&client, &search_request));
     }
 
-    void add_search_service(const SearchService &search_service)
+    void add_search_service(SearchService &search_service)
     {
         const std::scoped_lock lock(m_search_services_lock);
         m_search_services.push_back(&search_service);
     }
 
+    void serve()
+    {
+        const std::scoped_lock lock(m_search_services_lock);
+        std::vector<std::thread> threads(m_search_services.size());
+        for (auto *search_service : m_search_services) {
+            //std::thread thread(&SearchProxy::handle_service_request, this, std::ref(search_service));
+            //threads.push_back(std::thread(&SearchProxy::handle_service_request, this, std::ref(search_service)));
+            threads.emplace_back([this, search_service] {
+                this->handle_service_request(*search_service);
+            });
+        }
+
+        for (auto &thread : threads) {
+            if (thread.joinable())
+                thread.join();
+        }
+
+        threads.clear();
+    }
+
 private:
-    std::vector<const SearchService *> m_search_services;
-    std::map<Client::SubscriptionType, std::priority_queue<SearchTask, std::vector<SearchTask>, SearchTaskCompare>> m_job_queues;
+    /**
+     * We declare random engine instance and uniform random generator as members
+     * because declaring them as static variables on a concurrent method is not
+     * thread-safe and would require extra synchronization.
+     */
+    std::default_random_engine m_random_engine;
+    std::uniform_real_distribution<float> generate_random_float;
+
+    std::vector<SearchService *> m_search_services;
+    std::map<Client::SubscriptionType, std::priority_queue<SearchTask, std::vector<SearchTask>, SearchTaskCompare>> m_queues;
+    std::map<Client::SubscriptionType, std::mutex> m_queue_locks;
 
     /**
      * Not really needed (when using SearchProxy), but we are cautious enough
      * to lock content sources when modifying or iterating over.
      */
     std::mutex m_search_services_lock;
+
+    void handle_service_request(SearchService &search_service)
+    {
+        Client::SubscriptionType key;
+        if (generate_random_float(m_random_engine) <= 0.8)
+            key = Client::SubscriptionType::Premium;
+        else
+            key = Client::SubscriptionType::Standard;
+
+        Client *client_ptr;
+        SearchRequest const *search_request_ptr;
+        {
+            const std::scoped_lock lock(m_queue_locks[key]);
+            auto queue = m_queues[key];
+            if (queue.empty())
+                return;
+
+            std::tie(client_ptr, search_request_ptr) = queue.top();
+            queue.pop();
+        }
+
+        search_service.query(*client_ptr, *search_request_ptr);
+    }
 };
 }
