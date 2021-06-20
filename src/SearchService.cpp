c4929efd27470c23ad76cd63fccce6f1c62b7ee6
angel <angel@ttm.sh>
2021-06-21 00:38:47 +0200

    SearchService: add search algorithm

--- src/SearchService.cpp
+++ src/SearchService.cpp
@@ -18,8 +18,12 @@
 #include <iostream>
 #include <thread>
 
+#include <MTFind2/Client/Client.h>
+#include <MTFind2/Messages/NotEnoughCreditMessage.h>
+#include <MTFind2/Messages/SearchResultFoundMessage.h>
 #include <MTFind2/Search/SearchService.h>
-#include <MTFind2/Messages/NoSearchResultsFoundMessage.h>
+#include <Shared/TextHelper.h>
+#include <Shared/Semaphore.h>
 
 namespace mtfind2 {
 void SearchService::query(Client &client, const SearchRequest &search_request)
@@ -47,5 +51,42 @@ void SearchService::query(Client &client, const SearchRequest &search_request)
 
 void SearchService::find_in_source(const ContentSource &content_source, Client &client, const SearchRequest &search_request) const
 {
+    size_t line_number = 1;
+    std::string lowercase_query { search_request.query() };
+    TextHelper::transform_to_lowercase(lowercase_query);
+
+    for (const std::string &line : content_source.lines()) {
+        std::string lowercase_line { line };
+        TextHelper::transform_to_lowercase(lowercase_line);
+
+        for (auto [start_pos, end_pos] = std::make_tuple(0, 0);
+             start_pos != std::string::npos;) {
+            if (!client.has_credit()) {
+                Semaphore semaphore;
+                client.push_message(NotEnoughCreditMessage(semaphore));
+                if (client.subscription_type() == Client::SubscriptionType::Standard)
+                    return;
+
+                // Wait for credit recharge if user is premium
+                semaphore.wait();
+                std::cout << search_request << ": resuming search request after credit recharge" << std::endl;
+            }
+
+            std::tie(start_pos, end_pos) = TextHelper::find_in_string(lowercase_line, lowercase_query, end_pos);
+            if (start_pos == std::string::npos)
+                break;
+
+            bool is_final_result = false;
+            if (line_number == content_source.lines().size()) {
+                auto [next_start_pos, _] = TextHelper::find_in_string(lowercase_line, lowercase_query, end_pos);
+                is_final_result = next_start_pos == std::string::npos;
+            }
+
+            client.consume_credit();
+            client.push_message(SearchResultFoundMessage(search_request, SearchResult(content_source, TextHelper::get_surrounding_text(line, start_pos, end_pos), line_number, start_pos + 1, search_request.query().length(), is_final_result)));
+        }
+
+        line_number++;
+    }
 }
 }
